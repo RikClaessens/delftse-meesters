9095dc6e5c7b7911aedcedbb9f137f6186fb47c4
Rik Claessens <rik.claessens@exact.com>
2018-11-23 22:00:42 +0100

    comment unused code for now

--- src/Pages/Maestro25Mei.js
+++ src/Pages/Maestro25Mei.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import Page from './Page';
-import { HighlightText, ImageRow, Paragraph, Title } from '../components';
-import HeaderImage from '../assets/images/zomerconcert-khd.jpg';
-import Kapel from '../assets/images/kapel.jpg';
+// import Page from './Page';
+// import { HighlightText, ImageRow, Paragraph, Title } from '../components';
+// import HeaderImage from '../assets/images/zomerconcert-khd.jpg';
+// import Kapel from '../assets/images/kapel.jpg';
 
 const Maestro25Mei = () => <div />;
 
