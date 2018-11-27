8526f13dd2ed400e0748b7980526b827700dcac9
Rik Claessens <rik.claessens@exact.com>
2018-11-27 13:40:45 +0100

    scroll to top on route change

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { configureAnchors } from 'react-scrollable-anchor';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import ScrollToTop from './ScrollToTop';
 import HomePage from './Pages/HomePage';
 import KHDEnDeGoudenEeuw from './Pages/KHDEnDeGoudenEeuw';
 import DelftseMeesters25Mei from './Pages/DelftseMeesters11Mei';
@@ -17,33 +18,35 @@ configureAnchors({ scrollDuration: 800 });
 
 const App = () => (
   <Router>
-    <Switch>
-      <Route path="/nieuws" exact component={News} />
-      <Route
-        path="/nieuws/ideeen-muziekstuk-voor-delft"
-        exact
-        component={IdeeenMuziekstuk}
-      />
-      <Route
-        path="/khd-en-de-gouden-eeuw"
-        exact
-        component={KHDEnDeGoudenEeuw}
-      />
-      <Route
-        path="/delftse-meesters-11-mei"
-        exact
-        component={DelftseMeesters25Mei}
-      />
-      <Route
-        path="/concertfestival-25-mei"
-        exact
-        component={ConcertFestival25Mei}
-      />
-      <Route path="/maestro-25-mei" exact component={Maestro25Mei} />
-      <Route path="/contact" exact component={Contact} />
-      <Route path="/site" exact component={HomePage} />
-      <Route path="/" component={ComingSoon} />
-    </Switch>
+    <ScrollToTop>
+      <Switch>
+        <Route path="/nieuws" exact component={News} />
+        <Route
+          path="/nieuws/ideeen-muziekstuk-voor-delft"
+          exact
+          component={IdeeenMuziekstuk}
+        />
+        <Route
+          path="/khd-en-de-gouden-eeuw"
+          exact
+          component={KHDEnDeGoudenEeuw}
+        />
+        <Route
+          path="/delftse-meesters-11-mei"
+          exact
+          component={DelftseMeesters25Mei}
+        />
+        <Route
+          path="/concertfestival-25-mei"
+          exact
+          component={ConcertFestival25Mei}
+        />
+        <Route path="/maestro-25-mei" exact component={Maestro25Mei} />
+        <Route path="/contact" exact component={Contact} />
+        <Route path="/site" exact component={HomePage} />
+        <Route path="/" component={ComingSoon} />
+      </Switch>
+    </ScrollToTop>
   </Router>
 );
 
